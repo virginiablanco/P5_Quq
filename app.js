ae781648303fa1735d54f642fe0c4c4b673dbfac
virginiablanco <36532703+virginiablanco@users.noreply.github.com>
2018-05-19 17:10:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser'); //importar MWs utilizados compatibles con express
-var bodyParser = require('body-parser');
-//importar routers del directorio ./routers/...
-var index = require('./routes/index');
-var users = require('./routes/users');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var app = express(); //crear aplicación
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); //intala renderizados de vistas EJS
+app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(bodyParser.json()); //instala MWs que procesan partes de req o res
-//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-//app.use('/', routers); //AÑADIDO POR MI //instalar MW routers que atienden a las rutas específicas / y /users
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
